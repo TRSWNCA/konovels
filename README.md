e10ec622ba3310e4e7816027003fa8ec9445212f
Yichi Chen <alex_passenger@yeah.net>
2019-12-01 13:57:28 +0800

    修改：     README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # konovels
-Make a readable online novels for Kindle or Duokan etc.
+
+一个能下载小说到电子设备的应用
+
+- [x] Linux/Mac OS 支持
+- [] 多线程下载
+- [x] 模糊搜索
+- [] 多选格式(mobi, epub, txt)
+- [] 邮箱推送
+
+# 说明
+
+要求 `python ver >= 3.7`
+能自由调用 `pandoc`
+kindle用户请添加 `kindlegen`
+
+# 声明
+
+此仓库仅供学习使用，不储存、提供任何书籍，数据均来自互联网
+
--- sources/quanshu.py
+++ sources/quanshu.py
@@ -1,22 +1,23 @@
-from bs4 import BeautifulSoup as soup
 from urllib.request import quote
 
 import requests
+from bs4 import BeautifulSoup as soup
 
 
-def getAccurateName(book_name):
-    """fuzzy search supported"""
-    book_name = quote(book_name.strip().encode('utf-8'))
+def getAccurateName(sName):
+    """
+    fuzzy search supported
+    """
+    sName = quote(sName.strip().encode('utf-8'))
     QidianUrl = 'https://www.qidian.com/search?kw='
-    html = requests.get(QidianUrl + repr(book_name)[1:-1])
+    html = requests.get(QidianUrl + repr(sName)[1:-1])
     html.encoding = 'utf-8'
     bsObj = soup(html.text, features='lxml')
     search_resname = []
-    search_resurl = []
     for search_res in bsObj.findAll("h4"):
         search_resname.append(search_res.get_text())
-        search_resurl.append(search_res.contents[0]['href'])
-    for num, _ in enumerate(reversed(search_res)):
-        print("\033[1;35m [%02d] \033[0m %s" % (len(search_res) - num - 1, _))
+    for num, _ in enumerate(reversed(search_resname)):
+        print("\033[1;35m [%02d] \033[0m %s" %
+              (len(search_resname) - num - 1, _))
     resnum = input(">> 请选择结果: ")
     return search_resname[int(resnum)]
